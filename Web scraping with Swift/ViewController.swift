5c7942afe7a4eceea964818b6f6b34c6499e7128
Margels <89708428+Margels@users.noreply.github.com>
2022-10-27 17:47:23 +1100

    Add files via upload

---
+++ Web scraping with Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//
+//
+//  Created by Margels.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
